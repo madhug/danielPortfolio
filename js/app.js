ed02c91aeda67726d3f80974bd3b9d3c5632495f
mavarius <metalwhirlwind@yahoo.com>
2015-06-08 18:56:01 -0700

    Daniel Design
    This is the design work done thus far

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 $(function() {
     var skillset = {
-        'photoshop': 3,
-        'illustrator': 2,
-        'dreamweaver': 3,
-        'indesign': 4,
-        'fireworks': 3,
-        'flash': 4,
+        'photoshop': 12,
+        'illustrator': 9,
+        'dreamweaver': 9,
+        'indesign': 8,
+        'fireworks': 7,
+        'flash': 6,
         'cocos': 4,
-        'html-graph': 3,
-        'css-graph': 3,
-        'jquery-graph': 2,
-        'bootstrap-graph': 5,
-        'js-graph': 3,
-        'angularjs': 3
+        'html-graph': 10,
+        'css-graph': 10,
+        'jquery-graph': 5,
+        'bootstrap-graph': 3,
+        'js-graph': 2,
+        'angularjs': 2
     };
     
     _.each(skillset, function(value, key) {
